4cbb9906452f2f39426b71f2406a75ca4224b1eb
tombiagioni <tombiagioni1983@gmail.com>
2015-04-09 20:17:09 -0700

    Began Work on Custom DanmakuEngine/PoolBoss integration

---
+++ Assets/ShipceptionEngine/Scripts/StarKillerBulletCreator.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using VirtualDropkick.DanmakuEngine;
+using VirtualDropkick.DanmakuEngine.Unity;
+
+public class StarKillerBulletCreator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private DanmakuContext context;
+
+    void Awake()
+    {
+        AwakeOrSpawned();
+    }
+
+    void OnSpawned()
+    {
+        
+        AwakeOrSpawned();
+    }
+
+    void AwakeOrSpawned()
+    {
+        context = DanmakuController.Instance.GetContext("PlayerContext");
+
+        // register creation/destruction-delegates
+        context.BulletCreationHandler = CreateBullet;
+        context.BulletDestructionHandler = DestroyBullet;
+    }
+
+    private DanmakuBullet CreateBullet(BulletModel bulletModel)
+    {
+        // put your construction logic here...
+        BulletLibrary lib = context.bulletLibrary;
+        DanmakuBullet prefab = lib.GetBulletPrefab(bulletModel.Id);
+        DanmakuBullet bullet = (Instantiate(prefab) as DanmakuBullet);
+
+        return bullet;
+    }
+
+    private void DestroyBullet(DanmakuBullet bullet)
+    {
+        // put your destruction logic here...
+        Destroy(bullet.gameObject);
+    }
+}
---
+++ Assets/ShipceptionEngine/Scripts/StarKillerEmitterCreator.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using VirtualDropkick.DanmakuEngine;
+using VirtualDropkick.DanmakuEngine.Unity;
+
+
+public class StarKillerEmitterCreator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private DanmakuContext context;
+
+    void Awake()
+    {
+        context = DanmakuController.Instance.GetContext("MyContext");
+
+        // register creation/destruction-delegates
+        context.EmitterCreationHandler = CreateEmitter;
+        context.EmitterDestructionHandler = DestroyEmitter;
+    }
+
+    private DanmakuEmitter CreateEmitter(EmitterModel emitterModel)
+    {
+        // put your construction logic here...
+        BulletLibrary lib = context.bulletLibrary;
+        DanmakuEmitter prefab = lib.defaultEmitter.prefab;
+        DanmakuEmitter emitter = (Instantiate(prefab) as DanmakuEmitter);
+
+        return emitter;
+    }
+
+    private void DestroyEmitter(DanmakuEmitter emitter)
+    {
+        // put your destruction logic here...
+        Destroy(emitter.gameObject);
+    }
+}
