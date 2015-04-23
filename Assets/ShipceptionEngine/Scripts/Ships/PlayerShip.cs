975772ae0b1da34ef6ce139d372c0ecc0e156324
tombiagioni <tombiagioni1983@gmail.com>
2015-04-23 05:32:23 -0700

    Added GUI files

--- Assets/ShipceptionEngine/Scripts/Ships/PlayerShip.cs
+++ Assets/ShipceptionEngine/Scripts/Ships/PlayerShip.cs
@@ -18,37 +18,37 @@ namespace Shipception
 		public ShipComponent Engine;
 		public ShipComponent Cockpit;
 
-        public string customEventName = "";
+        public string CustomEventName = "";
 
 
 
-		public Sprite playerSprite;
-		private Vector2 movement;
+		public Sprite PlayerSprite;
+		//private Vector2 _movement;
 
-        public List<KeyCode> upKey;
-        public List<KeyCode> downKey;
-        public List<KeyCode> leftKey;
-        public List<KeyCode> rightKey;
-        public List<KeyCode> fireKey;
+        public List<KeyCode> UpKey;
+        public List<KeyCode> DownKey;
+        public List<KeyCode> LeftKey;
+        public List<KeyCode> RightKey;
+        public List<KeyCode> FireKey;
 
 	    //public Weapon Weapon;
 
-		public float moveSpeed = 6f;  //remove?      
-		public Rect movementBounds;
+		public float MoveSpeed = 6f;     
+		public Rect MovementBounds;
 
         // Movement modifier applied to directional movement.
-        public float playerSpeed = 2.0f;
+        public float PlayerSpeed = 2.0f;
 
         // What the current speed of our player is
         private float currentSpeed = 0.0f;
         // The last movement that we've made
         private Vector3 lastMovement = new Vector3();
 
-		private DanmakuContext context;
+		private DanmakuContext _context;
 
 
 
-		private Vector3 direction = new Vector3();
+		//private Vector3 direction = new Vector3();
 
 		private Transform _transform;
 
@@ -66,97 +66,62 @@ namespace Shipception
 	    private void AwakeOrSpawned()
 	    {
 	        _transform = transform;
-            context = DanmakuController.Instance.GetContext("Player");
+            _context = DanmakuController.Instance.GetContext("Player");
 	    }
 
 	    private void Update()
 		{
-            HandleRotation();
-			Movement();
+            //HandleRotation();
+			HandleMovement();
 			HandleShooting();
 
 		}
 
-
-
-
-
-        //private void HandleMovement()
-        //{
-        //    direction.Set(0f, 0f, 0f);
-
-        //    if (Input.GetKey(upKey))
-        //    {
-        //        direction.y = 1f;
-        //    }
-        //    else if (Input.GetKey(downKey))
-        //    {
-        //        direction.y = -1f;
-        //    }
-
-        //    if (Input.GetKey(leftKey))
-        //    {
-        //        direction.x = -1f;
-        //    }
-        //    else if (Input.GetKey(rightKey))
-        //    {
-        //        direction.x = 1f;
-        //    }
-
-        //    Vector2 newPosition = _transform.position +
-        //                         (moveSpeed * direction.normalized * context.DeltaTime);
-
-        //    if (newPosition.x < movementBounds.xMin)
-        //    {
-        //        newPosition.x = movementBounds.xMin;
-        //    }
-        //    else if (newPosition.x > movementBounds.xMax)
-        //    {
-        //        newPosition.x = movementBounds.xMax;
-        //    }
-
-        //    if (newPosition.y < movementBounds.yMin)
-        //    {
-        //        newPosition.y = movementBounds.yMin;
-        //    }
-        //    else if (newPosition.y > movementBounds.yMax)
-        //    {
-        //        newPosition.y = movementBounds.yMax;
-        //    }
-
-        //    _transform.position = newPosition;
-
-        //}
-
-		private void HandleShooting()
-		{
-		    this.GetComponent<Weapon>().Fire(fireKey);
-		}
-        // Will move the player based off of keys pressed
-        void Movement()
+        private void HandleMovement()
         {
+
             // The movement that needs to occur this frame
             Vector3 movement = new Vector3();
 
             // Check for input
-            movement += MoveIfPressed(upKey, Vector3.up);
-            movement += MoveIfPressed(downKey, Vector3.down);
-            movement += MoveIfPressed(leftKey, Vector3.left);
-            movement += MoveIfPressed(rightKey, Vector3.right);
+            movement += MoveIfPressed(UpKey, Vector3.up);
+            movement += MoveIfPressed(DownKey, Vector3.down);
+            movement += MoveIfPressed(LeftKey, Vector3.left);
+            movement += MoveIfPressed(RightKey, Vector3.right);
 
-            /*
-               * If we pressed multiple buttons, make sure we're only
-               * moving the same length.
-            */
             movement.Normalize();
+
+            Vector2 newPosition = _transform.position +
+                                 (MoveSpeed * movement * _context.DeltaTime);
+
+            //Constrain to In-Editor defined ScreenBounds
+            if (newPosition.x < MovementBounds.xMin)
+            {
+                newPosition.x = MovementBounds.xMin;
+            }
+            else if (newPosition.x > MovementBounds.xMax)
+            {
+                newPosition.x = MovementBounds.xMax;
+            }
+
+            if (newPosition.y < MovementBounds.yMin)
+            {
+                newPosition.y = MovementBounds.yMin;
+            }
+            else if (newPosition.y > MovementBounds.yMax)
+            {
+                newPosition.y = MovementBounds.yMax;
+            }
+
+            _transform.position = newPosition;
 
             // Check if we pressed anything
             if (movement.magnitude > 0)
             {
                 // If we did, move in that direction
-                currentSpeed = playerSpeed;
+                currentSpeed = PlayerSpeed;
                 this.transform.Translate(movement * Time.deltaTime *
-                                        playerSpeed, Space.World);
+                                        PlayerSpeed, Space.World);
                 lastMovement = movement;
             }
             else
@@ -167,12 +132,54 @@ namespace Shipception
                 // Slow down over time
                 currentSpeed *= .9f;
             }
+
         }
+
+		private void HandleShooting()
+		{
+		    this.GetComponent<Weapon>().Fire(FireKey);
+		}
+        // Will move the player based off of keys pressed
+        //private void Movement()
+        //{
+        //    // The movement that needs to occur this frame
+        //    Vector3 movement = new Vector3();
+
+        //    // Check for input
+        //    movement += MoveIfPressed(UpKey, Vector3.up);
+        //    movement += MoveIfPressed(DownKey, Vector3.down);
+        //    movement += MoveIfPressed(LeftKey, Vector3.left);
+        //    movement += MoveIfPressed(RightKey, Vector3.right);
+
+        //    /*
+        //       * If we pressed multiple buttons, make sure we're only
+        //       * moving the same length.
+        //    */
+        //    movement.Normalize();
+
+        //    // Check if we pressed anything
+        //    if (movement.magnitude > 0)
+        //    {
+        //        // If we did, move in that direction
+        //        currentSpeed = PlayerSpeed;
+        //        this.transform.Translate(movement * Time.deltaTime *
+        //                                PlayerSpeed, Space.World);
+        //        lastMovement = movement;
+        //    }
+        //    else
+        //    {
+        //        // Otherwise, move in the direction we were going
+        //        this.transform.Translate(lastMovement * Time.deltaTime
+        //                                * currentSpeed, Space.World);
+        //        // Slow down over time
+        //        currentSpeed *= .9f;
+        //    }
+        //}
         /*
 * Will return the movement if any of the keys are pressed,
 * otherwise it will return (0,0,0)
 */
-        Vector3 MoveIfPressed(List<KeyCode> keyList, Vector3 Movement)
+        Vector3 MoveIfPressed(List<KeyCode> keyList, Vector3 movement)
         {
             // Check each key in our list
             foreach (KeyCode element in keyList)
@@ -183,7 +190,7 @@ namespace Shipception
                       * It was pressed so we leave the function
                       * with the movement applied.
                     */
-                    return Movement;
+                    return movement;
                 }
             }
 
@@ -215,9 +222,9 @@ namespace Shipception
             // Assign the ship's rotation  
 	        this.transform.rotation = rot;
 
-            context.SetVariable("PlayerRotation", -angle - 90);
+            _context.SetVariable("PlayerRotation", -angle - 90);
 
-           // Debug.Log("#PlayerRotation = " + context.GetVariable("PlayerRotation"));
+           // Debug.Log("#PlayerRotation = " + _context.GetVariable("PlayerRotation"));
 
 
 	    }
